9f1000d4878fe5c2d41c26210ca75671b5ac086c
Maou <waspofcrimea@yandex.ru>
2022-11-16 21:13:20 +0300

    added math solvetion Task5

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -2,13 +2,21 @@
  и на выходе показывает вторую цифру этого числа.
  Обязательна проверка на ввод чисел больше/меньше 3-х знаков*/
 
- Console.WriteLine("Введите число : ");
+ Console.WriteLine("Введите трёхзначное число : ");
 int num1 = int.Parse (Console.ReadLine());
-string num = num1.ToString();
+/*string num = num1.ToString();
 if(num.Length !=3){
     Console.WriteLine("Неверное число!");
 }
 else
 {
     Console.WriteLine(num[1]);
+}*/
+if(num1 < 100 || num1 > 1000)
+{Console.WriteLine("Неверное число!");
 }
+else
+{
+    Console.WriteLine((num1%100)/10);
+}
+
