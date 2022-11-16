a72876b758b1aeb328c2d0ff64123007809fed03
Maou <waspofcrimea@yandex.ru>
2022-11-16 21:17:19 +0300

    solved Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача 13: Напишите программу, 
+которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+ Берем числа до 100000*/
+ Console.WriteLine("Введите число : ");
+int num1 = int.Parse (Console.ReadLine());
+string num = num1.ToString();
+if(num.Length < 3){
+    Console.WriteLine("Такого значения не существует");
+}
+else
+{
+    Console.WriteLine(num[2]);
+}
