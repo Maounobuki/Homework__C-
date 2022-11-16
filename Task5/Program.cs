4c0768516361670bf4de97f23225dc8f98ffb133
Maou <waspofcrimea@yandex.ru>
2022-11-16 20:26:02 +0300

    solved Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+ Обязательна проверка на ввод чисел больше/меньше 3-х знаков*/
+
+ Console.WriteLine("Введите число : ");
+int num1 = int.Parse (Console.ReadLine());
+string num = num1.ToString();
+if(num.Length !=3){
+    Console.WriteLine("Неверное число!");
+}
+else
+{
+    Console.WriteLine(num[1]);
+}
