24f884a4358b5d4344a150b264176c039fbf8737
Maou <waspofcrimea@yandex.ru>
2022-11-20 18:11:56 +0300

    Solved Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Таблица кубов до числа N: ");
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine(Math.Pow(i, 3));
+            }
