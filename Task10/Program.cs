a9040b105c1c33e0f7f18e84f6fa37dc895405b9
Maou <waspofcrimea@yandex.ru>
2022-11-24 21:12:13 +0300

    Solved task 11

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*Напишите программу, которая принимает на вход число (N) 
 и выдаёт таблицу кубов чисел от 1 до N.*/
 Console.WriteLine("Введите число N: ");
-            int N = int.Parse(Console.ReadLine());
+            int N = int.Parse(Console.ReadLine()!);
 
             Console.WriteLine("Таблица кубов до числа N: ");
             for (int i = 1; i <= N; i++)
---
+++ Task11/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+в натуральную степень B. 
+(Задачи, решенные через Math.Pow,
+ будут отправлены на переделку, так как задача стоит в том, чтобы написать цикл)*/
+
+Console.WriteLine("Введите основание: ");
+ int x = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите показатель степени: ");
+ int y = int.Parse(Console.ReadLine()!);
+ Console.WriteLine($"Результат: {Pow(x,y)}");
+
+//--------------------------------------------------------- method
+
+ int Pow(int basis, int rank)
+ {
+    int result = basis;
+    int count = 0;
+    while(count < rank - 1)
+    {
+result = result * basis;
+count++;
+    }
+    return result;
+ }
+
