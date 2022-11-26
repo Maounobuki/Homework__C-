37ebeaa3e9fac03104663c272dbc126925ea7c99
Maou <waspofcrimea@yandex.ru>
2022-11-26 19:13:00 +0300

    Solved task13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран. (числа берете любые)
+int[] array = new int[8];
+Console.WriteLine("Введите нижний предел: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите верхний предел: ");
+int b = int.Parse(Console.ReadLine()!);
+Mass(array, a, b);
+Console.WriteLine($"Массив:  [{String.Join(" , ",array)}]");
+
+int[] Mass(int[] array, int min, int max)
+{
+    for(int i=0; i<array.Length; i++){
+        array[i] = new Random().Next(min,max);
+    }
+    return array;
+} 
