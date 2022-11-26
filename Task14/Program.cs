175b6b1fa8acf0905205420037ea7026cbb6441b
Maou <waspofcrimea@yandex.ru>
2022-11-26 20:45:03 +0300

    Solved Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+
+Console.WriteLine("Введите размер массива: ");
+int a = int.Parse(Console.ReadLine()!);
+int [] array = new int [a];
+Mass(array);
+Console.WriteLine($"Массив:  [{String.Join(" , ",array)}]");
+Console.WriteLine($"Количество чётных элементов: {EvenCount(array)}");
+
+
+//-------------------methods--------------------------------
+
+
+
+int[] Mass(int[] array)
+{
+    for(int i=0; i<array.Length; i++){
+        array[i] = new Random().Next(100,1000);
+    }
+    return array;
+} 
+
+ 
+ int EvenCount(int[] Mass)
+ {
+    int count = 0;
+    int index = 0;
+   while(index<Mass.Length)
+    {
+   if(array[index]%2 == 0)
+   {
+    count++;
+    index++;
+   }
+   else
+   {
+    index++;
+   }
+    }
+    return count;
+ }
