eeeb5be83dd20f956b5b4292608afa505bc96204
Maou <waspofcrimea@yandex.ru>
2022-11-27 23:28:55 +0300

    Solved Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,60 @@
+﻿//Задача 38: Задайте массив целых чисел от -10 до 10. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+int[] array = new int[8];
+Mass(array);
+Div(array);
+Console.WriteLine($"Массив:  [{String.Join(" , ",array)}]");
+Console.WriteLine($"Разница между максимальным и минимальным: {Div(array)}");
+int[] Mass(int[] array)
+{
+    for(int i=0; i<array.Length; i++){
+        array[i] = new Random().Next(-10,10);
+    }
+    return array;
+} 
+
+int Div(int [] array)
+{
+int res = 0;
+int i = 0;
+int max = array[i];
+int min = array[i];
+while (i < array.Length)
+{
+ 
+ if(max < array [i])
+ {
+max = array[i];
+ }
+if(min > array [i])
+ {
+min = array[i];
+ }
+i++;
+}
+res = max - min;
+return res;
+}
+
+/*int res = 0;
+int i = 0;
+int max = array[i];
+int min = array[i];
+while (i < array.Length)
+{
+ 
+ if(max < array [i])
+ {
+max = array[i];
+ }
+if(min > array [i])
+ {
+min = array[i];
+ }
+i++;
+}
+res = max - min;
+Console.WriteLine($"{max}");
+Console.WriteLine($"{min}");
+Console.WriteLine($"{res}");*/
